c40f8ede0a6c756296ed8379eac532dbfe4f23b8
SkAfzal-04 <skmahammadjinnur@gmail.com>
2024-09-14 19:24:06 +0530

    backend commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios"); // Import axios
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "a7c018e0-7e3b-495f-87b8-d2e3ae080d74" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
