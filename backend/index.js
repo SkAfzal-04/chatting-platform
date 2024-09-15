aec8e97e1f6ff9ffc5078a3e67c21db9dde65510
Sk Mahammad Afzal <138036484+SkAfzal-04@users.noreply.github.com>
2024-09-15 15:58:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,8 @@ app.post("/authenticate", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+
 });
