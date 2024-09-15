974eb37f3087f902f467316b04db4d515dd1d05b
Sk Mahammad Afzal <138036484+SkAfzal-04@users.noreply.github.com>
2024-09-15 15:51:57 +0530

    Update AuthPage.jsx

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -4,7 +4,7 @@ const AuthPage = (props) => {
       e.preventDefault();
       const { value } = e.target[0];
       axios.post(
-        'http://localhost:3001/authenticate',
+        'https://chatting-platform.onrender.com/authenticate',
         {username:value}
     )
       .then(r=>props.onAuth({ ...r.data,secret:value }))
