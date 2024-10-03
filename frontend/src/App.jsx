a3e5e1983bf7bede17e198f3c93f39686bab5a59
Sk Mahammad Afzal <138036484+SkAfzal-04@users.noreply.github.com>
2024-10-03 16:42:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./App.css";
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+
+  const handleAuth = (userData) => {
+    setUser(userData); // Set the user state with the data received from AuthPage
+  };
+
   return (
     <div>
-      <h1>Afzal</h1>
+      {!user ? (
+        <AuthPage onAuth={handleAuth} />
+      ) : (
+        <ChatsPage user={user} />
+      )}
     </div>
   );
 }
