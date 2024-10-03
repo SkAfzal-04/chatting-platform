c28ef35d3c9fff210cfd65ddd1a72217c76dc025
Sk Mahammad Afzal <138036484+SkAfzal-04@users.noreply.github.com>
2024-10-03 15:50:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 
 // Alert to confirm main.jsx is hit
 alert('main.jsx is loaded!');
