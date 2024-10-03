00055c22d49d19f379e7cf96ef3a1950dae6003b
Sk Mahammad Afzal <138036484+SkAfzal-04@users.noreply.github.com>
2024-10-03 16:48:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
-import "./App.css";
-import AuthPage from "./AuthPage";
-import ChatsPage from "./ChatsPage";
+import React from 'react';
+import "./App.css"; // Ensure this path is correct
+import AuthPage from "./AuthPage"; // Ensure this path is correct
 
 function App() {
-  const [user, setUser] = useState(undefined);
-
-  const handleAuth = (userData) => {
-    setUser(userData); // Set the user state with the data received from AuthPage
-  };
-
   return (
     <div>
-      {!user ? (
-        <AuthPage onAuth={handleAuth} />
-      ) : (
-        <ChatsPage user={user} />
-      )}
+      <AuthPage />
     </div>
   );
 }
