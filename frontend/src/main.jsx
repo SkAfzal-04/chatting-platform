2c096656160fb16b3c960a64581f5db75873f92a
Sk Mahammad Afzal <138036484+SkAfzal-04@users.noreply.github.com>
2024-10-03 16:25:53 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// Alert to confirm main.jsx is hit
-alert('main.jsx is loaded!');
-const a=<div>afzal</div>
-ReactDOM.createRoot(document.getElementById('root')).render(a);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
